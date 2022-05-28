148362f3f5c69759185c3077f0d30b3e41bc29ab
Luz Castro <luz.c@misena.edu.co>
2022-05-27 19:32:16 -0500

    Configurando del repositorio

---
+++ Ejercicio_1.py
@@ -0,0 +1,6 @@
+print("***Bienvenido, vamos a validar como funciona una aplicación de control de versiones ***")
+print("***A continuación podra comprobar el resultado de diversas operaciones matematicas  ***")
+print(6 // 3)
+print(6 // 3.)
+print(6. // 3)
+print(6. // 3.)
